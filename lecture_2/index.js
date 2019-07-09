f306a9b48bcbb5bf41c69aaa82a1e8a0669df4c7
ext.adyakov <a.diakov@andersenlab.com>
2019-07-09 11:28:12 +0300

    api. pure html.

---
+++ lecture_2/index.js
@@ -0,0 +1,11 @@
+function getRandomColor() {
+	const letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+}
+setInterval(function () {
+	document.querySelector('h1').style.color = getRandomColor();
+}, 1000);
